520d67bb3bfcf0cf1a3b443a07389343a308647f
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2022-09-11 22:41:54 -0400

    Update turtle-functions.py

--- HW-9-12/turtle-functions.py
+++ HW-9-12/turtle-functions.py
@@ -1,3 +1,5 @@
+import turtle
+
 def hexagon(t,x,y,w,color,sidelen):
     t.penup()
     t.goto(x,y)
@@ -18,6 +20,18 @@ polly=turtle.Turtle()
 n= int(input("How many sides do you want in your polygon?"))
 l= int(input("What is the length of the sides?"))
 
+def ngon(t,x,y,w,color,sidelen):
+    t.penup()
+    t.goto(x,y)
+    t.width(w)
+    t.color(color)
+    t.pendown()
+        
+ngon(polly,0,0,1,"red",50)
+
 for i in range (n):
     polly.forward(l)
     polly.right(360/n)
+    
+
+turtle.exitonclick()
