88a73405dad9678a461a19e6158c185d39992d14
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2022-09-15 00:15:55 -0400

    Update answers.py

--- 9-15/answers.py
+++ 9-15/answers.py
@@ -1,3 +1,38 @@
+#Chapter 7 Answers
+#this is #7
+is_even= int(input("Enter a number: "))
+if is_even % 2 == 0:
+    print("True")
+else:
+    print("False")
+    
+    
+    
+#This is #8
+is_odd= int(input("Enter a number: "))
+if is_odd % 2 == 0:
+    print("False")
+else:
+    print("True")
+
+
+
+#This is #10
+def is_rightangled(a, b, c):
+    return abs((a ** 2 + b ** 2) - (c ** 2)) < 0.001
+
+#This is #11
+def is_rightangled(a, b, c):
+    if a > b and a > c:
+        return abs((b ** 2 + c ** 2) - (a ** 2)) < 0.001
+    elif b > a and b > c:
+        return abs((a ** 2 + c ** 2) - (b ** 2)) < 0.001
+    else:
+        return abs((a ** 2 + b ** 2) - (c ** 2)) < 0.001
+
+
+
+
 #CodingBat Answers
 
 #String_1---->hello_name
