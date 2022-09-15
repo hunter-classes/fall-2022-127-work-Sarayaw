958ce73802bcf7ba2eba9c6c9fee6b18af61d6de
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2022-09-14 23:46:50 -0400

    Create answers.py

---
+++ 9-15/answers.py
@@ -0,0 +1,13 @@
+#CodingBat Answers
+
+#String_1---->hello_name
+def hello_name(name):
+    return "Hello " + name + "!"
+  
+#String_1---->make_out_word
+def make_out_word(out, word):
+    return out[:2] + word + out[2:]
+  
+#String_1----->first_two
+def first_two(str):
+    return str[:2]
