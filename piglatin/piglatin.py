c425f00e52344f6f9ccbd6da0b8f3624e08749ed
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2022-09-18 19:21:32 -0400

    Update piglatin.py

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -1,7 +1,7 @@
 def main():
-    words = str(input("Input Sentence:")).split()
+    words = str(input("Input a word or sentence:")).split()
     for word in words:
         print(word[1:] + word[0] + "ay", end = " ")
-print ()
+    print ()
 
 main()
