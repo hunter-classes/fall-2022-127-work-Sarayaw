515b4362778c18bd2b5121a1eed29192f5a2988d
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2022-10-13 03:48:25 -0400

    Create lists.py

---
+++ listlab.py/lists.py
@@ -0,0 +1,30 @@
+#1
+a=[-3, 2, 1,19, 29,0]
+
+smallest = a[0] if a else None
+
+for i in a:
+    if i<smallest:
+        smallest=i
+        
+print("The smallest number is:", smallest)
+
+
+#2
+numbers=[5,7,2,1,8,4]
+
+for num in numbers:
+    if num % 2 !=0:
+        print("The odd number(s) is/are:",num)
+        
+        
+      
+#3
+st1 = ["Hello", "ur mom", "Bye", "bruh"]
+
+for i in range(len(list1)):
+    list1[i] = list1[i].upper()
+
+print(list1)
+
+#4
